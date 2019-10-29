4defe35ab54966346f22794913dc45f516db8999
davidsantander9 <davidsantander9@gmail.com>
2019-10-29 00:31:46 -0600

    Add layauts, component header, body, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyContentComponent } from './body-content/body-content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
